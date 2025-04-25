28cc86b8b12d195d92fc044d1fa9278cfaf38bc6
RomanWind <romanrozhok@op.pl>
2025-04-25 19:10:25 +0200

    Base Player Animations
    Created player movement, and player idle animations. Created player movement script responsible for player movement and animation control.

--- 2D RPG/Assets/Scripts/Player/PlayerMovement.cs
+++ 2D RPG/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,14 +3,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private readonly int moveX = Animator.StringToHash("MoveX");
+    private readonly int moveY = Animator.StringToHash("MoveY");
+    private readonly int moving = Animator.StringToHash("Moving");
+        
     private float speed = 5f;
     private PlayerActions actions;
     private Rigidbody2D rb2D;
+    private Animator animator;
     private Vector2 moveDirection;
 
     private void Awake()
     {
         actions = new PlayerActions();
+        animator = GetComponent<Animator>();
         rb2D = GetComponent<Rigidbody2D>();
     }
 
@@ -32,6 +38,14 @@ public class PlayerMovement : MonoBehaviour
     private void ReadMovement()
     {
         moveDirection = actions.Movement.Move.ReadValue<Vector2>().normalized;
+        if (moveDirection == Vector2.zero)
+        {
+            animator.SetBool(moving,false);
+            return;
+        }
+        animator.SetBool(moving, true);
+        animator.SetFloat(moveX, moveDirection.x);
+        animator.SetFloat(moveY, moveDirection.y);
     }
 
     private void OnEnable()
