7d4a7dcba278050a8cdd61ee9de0b8132372476a
RomanWind <romanrozhok@op.pl>
2025-04-27 20:59:20 +0200

    Player respawn system
    Game Manager created, player now can be respawned afrer death

--- 2D RPG/Assets/Scripts/Player/Player.cs
+++ 2D RPG/Assets/Scripts/Player/Player.cs
@@ -1,9 +1,21 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [Header("Config")]
     [SerializeField] private PlayerStats stats;
-    
+    private PlayerAnimations animations;
     public PlayerStats Stats => stats;
+
+    private void Awake()
+    {
+        animations = GetComponent<PlayerAnimations>();
+    }
+    
+    public void ResetPlayer()
+    {
+        stats.ResetPlayer();
+        animations.ResetPlayer();
+    }
 }
