8438131edd062c47ed16a7a8cf3fde8ea86e945c
RomanWind <romanrozhok@op.pl>
2025-05-28 17:21:39 +0200

    Set up Cinemachine camera and enemy attack decision system
    Cinemachine added.
    Implemented enemy attack decision logic: when an enemy reaches a certain distance from the player in a Chase state, it transitions into an Attack state.

---
+++ 2D RPG/Assets/Scripts/Enemy/FiniteStateMachine/Decisions/DecisionAttackRange.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class DecisionAttackRange : FSMDecision
+{
+    [FormerlySerializedAs("range")]
+    [Header("Config")]
+    [SerializeField] private float attackRange;
+    [SerializeField] private LayerMask playerMask;
+    private EnemyBrain enemy;
+
+    private void Awake()
+    {
+        enemy = GetComponent<EnemyBrain>();
+    }
+
+    public override bool Decide()
+    {
+        return PlayerInAttackRange();
+    }
+
+    private bool PlayerInAttackRange()
+    {
+        if (enemy.Player == null) return false;
+        
+        Collider2D playerCollider = Physics2D.OverlapCircle(enemy.transform.position, attackRange, playerMask);
+        if (playerCollider != null)
+            return true;
+        
+        return false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+}
